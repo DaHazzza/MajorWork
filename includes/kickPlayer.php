b189584f85ee2259dd9e6e142d90d9870cd7f658
DaHazza <59498991+DaHazzza@users.noreply.github.com>
2022-05-12 20:33:00 +1000

    if a captin leaves the team a random player in the team will be promoted

--- includes/kickPlayer.php
+++ includes/kickPlayer.php
@@ -5,8 +5,19 @@ $uid = $_POST['playerID'];
 $tid = $_POST['teamID'];
 echo $tid;
 
+
+
+
 $sql = 'UPDATE users SET teamID = 0 WHERE id ='.$uid.';';
 $result = mysqli_query($conn,$sql);
+$teamInfo = getTeamInfo($conn,$tid);
+$players = GetPlayerNamesFromTeamID($conn,$tid);
+if ($teamInfo['captinID'] == $uid){
+    print_r($players);
+    $newCap = array_search( array_values($players)[0],$players);
+    $sql = 'UPDATE teams SET captinID = '.$newCap.' WHERE teamID ='.$tid.';';
+    $result = mysqli_query($conn,$sql);
+}
 $players = GetPlayerNamesFromTeamID($conn,$tid); #if no more plaers the team will be deleted
 if ($players == false){
     delTeam($conn,$tid);
