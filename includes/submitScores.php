3de51a2dd88013745607fb03bfff25e7f88cc2d9
DaHazza <59498991+DaHazzza@users.noreply.github.com>
2022-07-26 14:00:39 +1000

    Update submitScores.php

--- includes/submitScores.php
+++ includes/submitScores.php
@@ -1,3 +1,5 @@
+GNU nano 4.8                                                                                                    submitScores.php                                                                                                     Modified  
+
 <?php
 include 'database.php';
 include 'functions.php';
@@ -11,18 +13,18 @@ if(isset($_POST['t1Score']) and isset($_POST['t2Score']) and isset($_POST['id'])
     $t2Conf = $matchInfo['team2Confirm'];
     $t1Cap = getTeamInfo($conn, $matchInfo['team1ID'])['captinID'];
     $t2Cap = getTeamInfo($conn, $matchInfo['team2ID'])['captinID'];
-    if(isset($_SESSION['userID']) and ($_SESSION['userID'] == $t1Cap or $_SESSION['userID'] == $t2Cap)){
+    if(isset($_SESSION['userID']) and (($_SESSION['userID'] == $t1Cap and $t1Conf == 0) or ($_SESSION['userID'] == $t2Cap and $t2Conf == 0))){
         $confirmState  = $matchInfo['team1Confirm'] + $matchInfo['team2Confirm'];
         $header;
         if($_SESSION['userID'] == $t1Cap){
             $confirm = "team1Confirm = '1', team2Confirm =' 0'";
         }else{
-            $confirm = "team2Confirm = '1', team1Confirm = '0'";
+            $confirm = "team2Confirm = '1', team1Confirm = '0'";     
         }       
         if($confirmState == 0){
             $header = "wait";
             $sql = 'UPDATE matches SET team1Score = "'.$team1Score.'", team2Score = "'.$team2Score.'", '.$confirm.'  WHERE matchID = "'.$_POST['id'].'";';
-            
+
         }elseif($confirmState == 1){
             if($team1Score == $matchInfo['team1Score'] and $team2Score == $matchInfo['team2Score']  ){
                 $sql = 'UPDATE matches SET '.$confirm.', completed = 1  WHERE matchID = '.$_POST['id'];
